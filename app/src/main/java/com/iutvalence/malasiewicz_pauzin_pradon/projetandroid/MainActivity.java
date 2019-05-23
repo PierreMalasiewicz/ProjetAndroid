ff6f30c21fbc2d0617bbc6832e1d41e970ddd5ad
ZephyrArcturus <riep69.pm@gmail.com>
2019-05-23 12:00:31 +0200

    Initial commit

---
+++ app/src/main/java/com/iutvalence/malasiewicz_pauzin_pradon/projetandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iutvalence.malasiewicz_pauzin_pradon.projetandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
