d7c8dedbf2d4ada3337f40cff31f6e8abe837526
PierreMalasiewicz <pierre.malasiewicz@gmail.com>
2019-05-24 14:30:22 +0200

    Objet Json créé a partir du résultat de la requette

--- app/src/main/java/com/iutvalence/malasiewicz_pauzin_pradon/projetandroid/ProfileDetailsActivity.java
+++ app/src/main/java/com/iutvalence/malasiewicz_pauzin_pradon/projetandroid/ProfileDetailsActivity.java
@@ -3,11 +3,16 @@ package com.iutvalence.malasiewicz_pauzin_pradon.projetandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProfileDetailsActivity extends AppCompatActivity {
 
     String requestResponse;
+    JSONObject dataJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,23 @@ public class ProfileDetailsActivity extends AppCompatActivity {
 
         TextView json = findViewById(R.id.JsonTV);
         json.setText(requestResponse);
+
+        try {
+
+            dataJson = new JSONObject(requestResponse);
+
+            Log.d("My App", dataJson.toString());
+
+        } catch (Throwable t) {
+            Log.e("My App", "Could not parse malformed JSON: \"" + json + "\"");
+        }
+
+        try {
+            String truc = dataJson.get("level").toString();
+            Log.e("truc", truc);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
     }
